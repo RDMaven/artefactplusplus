572d784fbd5312cd5d41b12a77e7bfafcbaa9e5f
Max Joseph-Antoine <max.joseph-antoine@telecom-paris.fr>
2026-02-28 23:03:37 +0100

    (BIG) www frontend : joystick importé ! + récupération et envoie des valeurs x/y du joystick au server, le joystick se cache en mode auto sur la page

--- www/assets/scripts/app.js
+++ www/assets/scripts/app.js
@@ -1,56 +1,132 @@
 
 import { onScreenLog, consoleClear } from './log.js';
-import { ws } from './ws.js';
+import { ws, sendWSMessage } from './ws.js';
+import VirtualJoystick from "./imports/virtual-joystick.js";
 
-// WS : bouton de test
+
+/* ===================================================== */
+/* JOYSTICK CREATION =================================== */
+/* ===================================================== */
+const joystickContainer = document.querySelector('.joystick');
+const joystick = new VirtualJoystick(joystickContainer, {
+  // width: 80,
+  // height: 80,
+  color: "gray",
+  handleColor: "white",
+  handleRadius: 25,
+  onChange: (data) => {
+    updateJoystickValues(data);
+  },
+  onStart: null,
+  onEnd: null,
+  sensitivity: 1,
+  boundaries: true,
+  autoCenter: true,
+  deadzone: 0.1,
+  shape: "circle",
+  mode: "static", // 'static' or 'dynamic'
+  lockAxis: null, // null, 'x', or 'y'
+  zones: [],
+  vibration: true,
+  theme: {
+    base: {
+      background: "rgba(70, 70, 70, 0.5)",
+      border: "3px solid rgba(0, 0, 0, 0.8)",
+      shadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+    },
+    handle: {
+      background: "rgba(255, 255, 255, 0.9)",
+      border: "2px solid rgba(0, 0, 0, 0.8)",
+      shadow: "0 0 5px rgba(0, 0, 0, 0.5)",
+    },
+  },
+  keyboardEmulation: {
+    enabled: false,
+    map: {
+      up: "ArrowUp",
+      down: "ArrowDown",
+      left: "ArrowLeft",
+      right: "ArrowRight",
+    },
+  },
+  maxMoveRadius: null,
+});
+
+// To make sure the joystick has the right dimentions on display
+window.dispatchEvent(new Event('resize'));
+
+
+/* ===================================================== */
+/* WEBSOCKET FUNCTIONNALITIES ========================== */
+/* ===================================================== */
+
+// WS TESTER - Event listener
 $('#ws-test-button').on("click", () => {
-    ws.send("Hello from JS");
+  ws.send("Hello from JS");
 })
 
+// WS x Joystick : Update and Send joystick data to server
+var lastX = null;
+var lastY = null;
 
-// WS : controle manuel des robots
-const directions = {
-    up:    [ 0,  1],
-    down:  [ 0, -1],
-    left:  [-1,  0],
-    right: [ 1,  0]
-};
+function updateJoystickValues(data) {
 
-$.each(directions, function(direction, [x, y]) {
-    $(`#arrow-${direction}`).on('click', function() {
-        ws.send(JSON.stringify({ event: 'move', x, y }));
-    });
-});
+  // Update overlay stats in index.html
+  const statX = document.getElementById("stat-x");
+  const statY = document.getElementById("stat-y");
+  const x = +data.delta.x.toFixed(1);
+  const y = +data.delta.y.toFixed(1);
+  
+    // If both values are unchanged, do nothing
+  if (x === lastX && y === lastY) return;
+
+  lastX = x;
+  lastY = y;
+
+  sendWSMessage("move", x, y);
+
+  if (statX) statX.textContent = data.delta.x.toFixed(2);
+  if (statY) statY.textContent = data.delta.y.toFixed(2);
+
+}
+
+// STOP Button (TODO)
 
 
-// Toggle button
+// Mode switch Button
+var currentMode = 'Manual';
 
-$(document).ready(function () {
+$("#btnMode").on("change", function () {
+  currentMode = 'Manual';
+  if ($(this).is(":checked")) {
 
-  $("#btnMode").on("change", function () {
-    var current_mode = 'Manual';
-    if ($(this).is(":checked")) {
-
-      if (!confirm("Are you sure you want to switch to Auto mode? (*FLASHBANG INCOMING*")) {
-        $(this).prop("checked", false); // revert toggle
-        return;
-      }
-      current_mode = 'Auto';
-
-    } else {
+    // Message de confirmation
+    if (!confirm("Are you sure you want to switch to Auto mode? (*FLASHBANG INCOMING*")) {
+      $(this).prop("checked", false); // revert toggle
+      return;
     }
-    onScreenLog(`Switched to ${current_mode} Mode`, "error");
-
-  });
+    currentMode = 'Auto';
+  }
+  sendWSMessage("mode", currentMode.toLowerCase());
+  onScreenLog(`Switched to ${currentMode} Mode`, "error");
 
 });
 
 
-$('#btnMode').on('change', function() {
+/* ===================================================== */
+/* OTHER FUNCTIONNALITIES ============================== */
+/* ===================================================== */
+
+// Change the theme on mode toggle...
+$('#btnMode').on('change', function () {
   $('body').toggleClass('light-mode');
+  $('.joystick-container').toggle();
+
 });
 
-// Console Clear event listener (requires ws state):
+// Console Clear event listener (requires ws state, so thats why its in app.js and not log.js, sorry):
 $(".console-btn").on("click", function () {
-    consoleClear(ws.readyState);
+  consoleClear(ws.readyState);
 })
+
+
