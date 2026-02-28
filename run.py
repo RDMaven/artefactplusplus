9b59cc48c3fc503fa03e12596bbcacfda92964ad
Max Joseph-Antoine <max.joseph-antoine@telecom-paris.fr>
2026-02-28 22:56:42 +0100

    Déplacement du fichier principal www vers www/main.py. Corrections dans run.py

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 from config import Config
-from www import app
-from fastapi import FastAPI
-import os
-
+from www.main import app
+import uvicorn
 
 if __name__ == "__main__":
-    import uvicorn
-    
     uvicorn.run(
             app, 
             host=Config.Web.HOST, 
