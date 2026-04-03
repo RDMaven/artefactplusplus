37691e5b6e6328ae9388091c51fbdf5ecac32368
Motte Thibaut <thibaut.motte@telecom-paris.fr>
2026-04-03 11:38:13 +0200

    hall sensor

--- sensors/start_sensor.sh
+++ sensors/start_sensor.sh
@@ -3,5 +3,22 @@
 echo "====================="
 echo "Lancement de l'environnement des capteurs"
 source ./sens/bin/activate
+
+
 pip install -r requirements.txt
 echo "====================="
+
+echo "====================="
+
+if command -v python3 >/dev/null 2>&1; then
+    PYTHON=python3
+elif command -v python >/dev/null 2>&1; then
+    PYTHON=python
+elif command -v py >/dev/null 2>&1; then
+    PYTHON=py
+else
+    echo "Python not found"
+    exit 1
+fi
+
+"$PYTHON" Hall_sensor.py
