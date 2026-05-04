d5a367cd4e51b06077792a53054c91d4ba10a38d
Motte Thibaut <thibaut.motte@telecom-paris.fr>
2026-05-04 18:19:47 +0200

    pip -> python

--- client/src/sensors/start_sensor.sh
+++ client/src/sensors/start_sensor.sh
@@ -8,20 +8,7 @@ source ./sens/bin/activate
 
 echo "====================="
 
-
-
-if command -v python3 >/dev/null 2>&1; then
-    PYTHON=python3
-elif command -v python >/dev/null 2>&1; then
-    PYTHON=python
-elif command -v py >/dev/null 2>&1; then
-    PYTHON=py
-else
-    echo "Python not found"
-    exit 1
-fi
-
-"$PYTHON" -m pip install -r ./requirements.txt
+python3 -m pip install -r ./requirements.txt
 
 echo "====================="
-"$PYTHON" MPU_9250_6500.py
+python3 MPU_9250_6500.py
