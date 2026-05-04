42e38b113cc7d9be8e23fc3c23c13c9f3839ca3c
Motte Thibaut <thibaut.motte@telecom-paris.fr>
2026-05-04 18:22:05 +0200

    Suppression de l'environnement sens

--- client/src/sensors/start_sensor.sh
+++ client/src/sensors/start_sensor.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
-echo "====================="
-echo "Lancement de l'environnement des capteurs"
-source ./sens/bin/activate
-
-
-
 echo "====================="
 
-python3 -m pip install -r ./requirements.txt
+
+
+if command -v python3 >/dev/null 2>&1; then
+    PYTHON=python3
+elif command -v python >/dev/null 2>&1; then
+    PYTHON=python
+elif command -v py >/dev/null 2>&1; then
+    PYTHON=py
+else
+    echo "Python not found"
+    exit 1
+fi
+
+"$PYTHON" -m pip install -r ./requirements.txt
 
 echo "====================="
-python3 MPU_9250_6500.py
+"$PYTHON" MPU_9250_6500.py
