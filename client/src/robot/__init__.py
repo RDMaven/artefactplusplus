7d19b013e4034e708db20a389afaf00dabc8fec6
Motte Thibaut <thibaut.motte@telecom-paris.fr>
2026-06-23 14:01:28 +0200

    on va essayer un robot youhou !

--- client/src/robot/__init__.py
+++ client/src/robot/__init__.py
@@ -1,11 +1,13 @@
 from src.robot.driver import RobotDriver
 from src.camera.camera import CameraMove
+from src.robot.kalodo import Kalodo
 
 # ======================================================= #
 # STARTUP =============================================== #
 # ======================================================= #
 robot = RobotDriver()
 camera = CameraMove()
+kalodo = Kalodo()
 
 # camera.demo(1)
 
---
+++ client/src/robot/kalodo.py
@@ -0,0 +1,7 @@
+from src.sensors.kalmanFilter.kalman import Kalman
+
+class Kalodo():
+    def __init__(self, kalman : Kalman, ):
+
+
+
