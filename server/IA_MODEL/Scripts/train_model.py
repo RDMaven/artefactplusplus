d87949f2336db51aa86d1c7f96cc6c93e74d147f
Eden <eva.prevot@telecom-paris.fr>
2026-06-23 11:28:51 +0200

    Achitecture pour YOLO training

---
+++ server/IA_MODEL/Scripts/train_model.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# import os
+# import time
+# import random
+# import pandas as pd
+# import cv2
+# from tqdm.auto import tqdm
+# from PIL import Image
+# import shutil
+
+model = YOLO('yolov8n.pt') 
+model.train(data='dataset/data.yaml', epochs=100, imgsz=320)
