870c7d2bdd2883a38c5126d185a8424dc6d80ccb
Eden <eva.prevot@telecom-paris.fr>
2026-06-25 15:44:32 +0200

    import detector dans logic

--- server/control_logic/main_auto_traque.py
+++ server/control_logic/main_auto_traque.py
@@ -1,10 +1,16 @@
-from server.IA_MODEL.Scripts.Detection import detector 
+from pathlib import Path
+
+SERVER_DIR = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(SERVER_DIR))
+
+from IA_MODEL.Scripts.Detection import detector 
+from www.routes.utils.utils_video import *
 
 last_seen_time = 0
 target_position = None
 
 while last_seen_time < 30:
-    frame =
+    frame = FrameStore.get_frame
 
 
 
